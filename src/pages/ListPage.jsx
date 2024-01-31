7ea515992e6142405a85ff4afed6d8d46836bb36
zexcv <krusher13@o2.pl>
2024-01-31 13:19:07 +0100

    Fetch Fix

--- src/pages/ListPage.jsx
+++ src/pages/ListPage.jsx
@@ -3,46 +3,58 @@ import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faMagnifyingGlass,
-  faPlus,
-  faMinus,
-  faStar,
+  // faPlus,
+  // faMinus,
+  // faStar,
 } from "@fortawesome/free-solid-svg-icons";
 
 export default function ListPage() {
-  const [movies, setMovies] = useState([]);
   const [moviesD, setMoviesD] = useState([]);
   const [found, setFound] = useState(false);
   const inputRef = useRef("");
-  let api_key = "e1f6fd51"
-  // let fetchDetails = `https://www.omdbapi.com/?apikey=e1f6fd51&i=${movie.imdbID}`
-
-  console.log(moviesD);
+  let api_key = "e1f6fd51";
+  
+  function keyPress(event){
+    if(event.key === "Enter"){
+      searchSelection()
+    }
+  }
 
   function searchSelection() {
-    let filmy = []
-
+    let films = [];
+  
     fetch(`http://www.omdbapi.com/?apikey=${api_key}&s=${inputRef.current.value}`)
       .then((res) => res.json())
       .then((data) => {
         if (data.Response === "True") {
-          setMovies(data.Search);
-          movies.forEach(movie => {
+          films = data.Search;
+  
+          const fetchPromises = films.map((movie) =>
             fetch(`https://www.omdbapi.com/?apikey=e1f6fd51&i=${movie.imdbID}`)
-              .then(res => res.json())
-              .then(data => {
-                filmy.push(data)
-                setMoviesD(filmy)
-              })
-          })
+              .then((res) => res.json())
+          );
+  
+          Promise.all(fetchPromises)
+            .then((moviesData) => {
+              setMoviesD(moviesData);
+            })
+            .catch((error) => {
+              console.error("Error fetching movie details:", error);
+            });
+  
           setFound(true);
         } else {
           setFound(false);
         }
+      })
+      .catch((error) => {
+        console.error("Error fetching search results:", error);
       });
   }
+  
 
   const moviesEls = moviesD.map((movie) => (
-    <div className="movie-component">
+    <div key={movie.imdbID} className="movie-component">
       <div className="movie">
         <div className="poster">
           <img src={movie.Poster} alt="poster" />
@@ -58,7 +70,7 @@ export default function ListPage() {
   ));
 
   return (
-    <section>
+    <section onKeyDown={keyPress}>
       <div className="header">
         <div className="header-text">
           <div className="find-text">Find your film</div>
@@ -86,3 +98,28 @@ export default function ListPage() {
     </section>
   );
 }
+
+
+// function searchSelection() {
+//   let films = []
+//   let filmsInfo = []
+
+//   fetch(`http://www.omdbapi.com/?apikey=${api_key}&s=${inputRef.current.value}`)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       if (data.Response === "True") {
+//         films = data.Search
+//         films.forEach((movie) => {
+//         fetch(`https://www.omdbapi.com/?apikey=e1f6fd51&i=${movie.imdbID}`)
+//           .then((res) => res.json())
+//           .then(data => {
+//             filmsInfo.push(data)
+//           });
+//         });
+//         setFound(true);
+//         console.log(filmsInfo)
+//       } else {
+//         setFound(false);
+//       }
+//     });
+// }
